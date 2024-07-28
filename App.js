c6b10ee310aee1702be30206386d6824a55d2633
Pragnyat <pragnyat9437@gmail.com>
2024-07-28 19:50:05 +0530

    first pull

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import GettingStarted from './screens/GettingStarted';
+import SignUp from './screens/SignUp';
+import Login from './screens/Login';
+import Welcome from './screens/Welcome';
+import CategorySelection from './screens/CategorySelection';
+import Products from './screens/Products';
+import Favorites from './screens/Favorites';
+import { UserProvider } from './screens/UserContext';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <UserProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="GettingStarted">
+          <Stack.Screen name="GettingStarted" component={GettingStarted} />
+          <Stack.Screen name="SignUp" component={SignUp} />
+          <Stack.Screen name="Login">
+            {props => <Login {...props} setIsLoggedIn={setIsLoggedIn} />}
+          </Stack.Screen>
+          <Stack.Screen name="Welcome">
+            {props => <Welcome {...props} setIsLoggedIn={setIsLoggedIn} />}
+          </Stack.Screen>
+          <Stack.Screen name="CategorySelection" component={CategorySelection} options={{ title: 'CategorySelection' }} />
+          <Stack.Screen name="Products" component={Products} options={{ title: 'Products' }} />
+          <Stack.Screen name="Favorites" component={Favorites} options={{ title: 'Favorites' }}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </UserProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
